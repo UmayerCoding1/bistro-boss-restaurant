08f786f76cb9a93eb815dc8527e4fc520c931a7d
unknown <umayerpro175@gmail.com>
2024-10-01 10:29:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req , res) => {
+    res.send('bistro boss restaurant server is ready');
+})
+
+app.get('/umayer', async(req,res) => {
+    res.send({
+        name: 'Umayer Hossain',
+        age:19,
+        cgpa: 3.82,
+    })
+})
+
+app.listen(port, () => {
+    console.log(`This server is running PORT: ${port}`);
+    
+})
