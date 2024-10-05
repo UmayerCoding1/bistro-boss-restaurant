dc28f7b5976e4da6f59db2233fbf57ea0b4d9a00
unknown <umayerpro175@gmail.com>
2024-10-05 10:14:01 +0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,14 +12,48 @@ app.get('/', (req , res) => {
     res.send('bistro boss restaurant server is ready');
 })
 
-app.get('/umayer', async(req,res) => {
-    res.send({
-        name: 'Umayer Hossain',
-        age:19,
-        cgpa: 3.82,
-        ggo: 33
-    })
-})
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.34gmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+     const reviewsCollection = client.db('bistroDB').collection('reviews');
+     const menusCollection = client.db('bistroDB').collection('menu');
+    
+//   menus
+     app.get('/menus', async(req,res) => {
+        const result = await menusCollection.find().toArray();
+        res.send(result);
+     });
+
+//   review
+     app.get('/reviews', async(req,res) => {
+        const result = await reviewsCollection.find().toArray();
+        res.send(result);
+     })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log(`This server is running PORT: ${port}`);
